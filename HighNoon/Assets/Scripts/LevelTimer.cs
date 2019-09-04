f8a1f72efc9822403ded847eb1694cacec82e836
profawesum <profawesum@gmail.com>
2019-09-04 16:15:50 +1200

    Added in level Timer
    Timer needs to do something when it reaches 0

---
+++ HighNoon/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+
+    public float timer = 120;
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+        timerText.text = "Time Remaining: " + timer.ToString("F2");
+    }
+}
