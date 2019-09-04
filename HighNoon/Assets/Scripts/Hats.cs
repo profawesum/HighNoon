e6fb0035cf58c8a8307e4381de646484ccdb81eb
prof <profawesum@gmail.com>
2019-09-05 11:07:44 +1200

    Fixed collision errors

--- HighNoon/Assets/Scripts/Hats.cs
+++ HighNoon/Assets/Scripts/Hats.cs
@@ -46,7 +46,7 @@ public class Hats : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.tag == "Player1" || collision.tag == "Player2" || collision.tag == "Player3" || collision.tag == "Player4")
+        if (collision.tag == "Player1" && this.tag == "Hats" || collision.tag == "Player2" && this.tag == "Hats" || collision.tag == "Player3" && this.tag == "Hats" || collision.tag == "Player4" && this.tag == "Hats")
         {
             playerEquipped = true;
             gameObject.GetComponent<Rigidbody2D>().gravityScale = -20;
